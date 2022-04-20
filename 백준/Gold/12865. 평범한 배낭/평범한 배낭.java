ea9a40f0a5336bff9f65816fa612874f3f0cf591
김우원(Kim Woowon) <dyffh1031@gmail.com>
2022-04-20 18:17:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 252 ms, Memory: 18424 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] weight = new int[N+1];
+		int[] worth = new int[N+1];
+		int[] dp = new int[K+1];
+		
+		for(int i = 1; i <= N; i++) {
+			weight[i] = sc.nextInt();
+			worth[i] = sc.nextInt();
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = K; j >= weight[i]; j--) {
+				dp[j] = Math.max(dp[j], dp[j-weight[i]]+worth[i]);
+			}
+		}
+		System.out.println(dp[K]);
+		sc.close();
+	}
+
+}
