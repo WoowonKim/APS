e9cc7693a633aeff1c352a55b43135cfb124df15
김우원(Kim Woowon) <dyffh1031@gmail.com>
2022-04-20 20:47:51 +0900

    [Gold IV] Title: 가르침, Time: 244 ms, Memory: 17904 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,58 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
-	static int N, K;
-	static int ans = -1;
-	static boolean[] visited;
-	static String[] words;
-
+	static int N,K,ans;
+	static int[] word;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		K = sc.nextInt();
-		sc.nextLine();
-		words = new String[N];
-		for (int i = 0; i < N; i++) {
-			String word = sc.nextLine();
-			word = word.replaceAll("[a,n,t,i,c]", "");
-			words[i] = word;
+		word = new int[N];
+		for(int i=0; i<N; i++) {
+			String str = sc.next();
+			for(int j=0; j<str.length(); j++)word[i] |=1<<(str.charAt(j)-'a');
 		}
-		visited = new boolean[26]; // 각 알파벳을 배웠는지 체크
-		visited['a' - 'a'] = true;
-		visited['c' - 'a'] = true;
-		visited['i' - 'a'] = true;
-		visited['n' - 'a'] = true;
-		visited['t' - 'a'] = true;
-		
-		if(K < 5) ans = 0;
-		else backtracking(0, 0);
-		
+		int t = 0;
+		t |= 1<<('a'-'a');
+		t |= 1<<('n'-'a');
+		t |= 1<<('t'-'a');
+		t |= 1<<('c'-'a');
+		t |= 1<<('i'-'a');
+		recur(5,0,t);
 		System.out.println(ans);
+		
 	}
-
-	private static void backtracking(int idx, int len) {
-		if(len == K - 5) {
-            int cnt = 0;
-            for(int i = 0; i < N; i++) {
-                boolean flag = true;
-                for(int j = 0; j < words[i].length(); j++) {
-                    if(!visited[words[i].charAt(j) - 'a']) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag) cnt++;
-            }
-            ans = Math.max(ans, cnt);
-            return;
-        }
-        
-        for(int i = idx; i < 26; i++) {
-            if(visited[i] == false) {
-                visited[i] = true;
-                backtracking(i, len + 1);
-                visited[i] = false;
-            }
-        }
+	
+	static void recur(int n, int idx, int v) {
+		if(n == K) {
+			int cnt = 0;
+			for(int i=0; i<N; i++)if((word[i] & v) == word[i])cnt++;
+			ans = Math.max(ans, cnt);
+			return;
+		}
+		else if(n > K)return;
+		for(int i=idx; i<26; i++)if((v&1<<i) == 0) {
+			recur(n+1,i,v|1<<i);
+		}
 	}
 }
