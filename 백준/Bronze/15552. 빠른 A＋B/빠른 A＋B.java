dbaa1fe77868a1ab6faaf1a4db35f21a9acdca82
김우원(Kim Woowon) <dyffh1031@gmail.com>
2022-05-14 13:31:53 +0900

    [Bronze II] Title: 빠른 A+B, Time: 860 ms, Memory: 221872 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		while(T-- > 0) {
+			StringTokenizer stk = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(stk.nextToken());
+			int b = Integer.parseInt(stk.nextToken());
+			sb.append(a+b).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
