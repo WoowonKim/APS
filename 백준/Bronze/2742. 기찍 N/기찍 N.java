db8bb57b1588ccef8aa1dde640e6f59a3d47bd39
김우원(Kim Woowon) <dyffh1031@gmail.com>
2022-05-14 13:36:14 +0900

    [Bronze III] Title: 기찍 N, Time: 224 ms, Memory: 18048 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for(int i = T; i >= 1; i--) {
+			sb.append(i).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
