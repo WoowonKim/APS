116a763bbec1f99856a7ccfc9823867cd55d4269
김우원(Kim Woowon) <dyffh1031@gmail.com>
2022-05-14 21:14:15 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		char car = sc.next().charAt(0);
+		System.out.println(car-0);
+	}
+}
