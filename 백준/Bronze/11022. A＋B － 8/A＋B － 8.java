b2fda0cab713384428c67eb245f70dec5c5ba615
김우원(Kim Woowon) <dyffh1031@gmail.com>
2022-05-14 13:41:58 +0900

    [Bronze III] Title: A+B - 8, Time: 280 ms, Memory: 19012 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int idx = 1;
+		while(T-- > 0) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println("Case #"+ idx++ + ": "+a + " + " + b +" = "+ (a + b));
+		}
+	}
+}
