f07fb02ef9abbe47ccead4c74c002adbafdfc4e2
김우원(Kim Woowon) <dyffh1031@gmail.com>
2022-05-23 20:33:30 +0900

    [Silver II] Title: 부분수열의 합, Time: 216 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+	static int ans;
+	static int N, S;
+	static int[] arr;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		S = sc.nextInt();
+		arr = new int[N];
+		for(int i = 0;  i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		partSum(0,0);
+		if(S==0) ans--;
+		System.out.println(ans);
+		
+	}
+	
+	private static void partSum(int idx, int sum) {
+		// TODO Auto-generated method stub
+		if(idx == N) {
+			if(sum==S) ans++;
+			return;
+		}
+		partSum(idx+1, sum + arr[idx]);
+		partSum(idx+1, sum);
+		
+	}
+
+
+}
