b3cf53cfb3efdbbb76f60c32e1c4bcedcc232777
김우원(Kim Woowon) <dyffh1031@gmail.com>
2023-01-09 16:55:55 +0900

    [unrated] Title: 디펜스 게임, Time: 4.93 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -1,19 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(;answer < enemy.length; answer++) {
-            n -= enemy[answer];
-            pq.add(enemy[answer]);
-            if(n < 0) {
-                if(k > 0 && !pq.isEmpty()){
-                    n += pq.poll();
-                    k--;
-                } 
-                else break;
-            }
-        }
         return answer;
     }
 }
