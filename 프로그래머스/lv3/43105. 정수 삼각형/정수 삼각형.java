12cb96e45639da9c04824d07ba8b463cce84dfd9
김우원(Kim Woowon) <dyffh1031@gmail.com>
2023-01-18 16:47:50 +0900

    [level 3] Title: 정수 삼각형, Time: 6.62 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        for(int i = triangle.length - 1; i > 0; i--) {
+            for(int j = 0; j < triangle[i].length - 1; j++) {
+                triangle[i-1][j] += Math.max(triangle[i][j], triangle[i][j+1]);
+            }
+        }
+        answer = triangle[0][0];
+        return answer;
+    }
+}
