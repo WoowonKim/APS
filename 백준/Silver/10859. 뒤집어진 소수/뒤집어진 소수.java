62bf2519f7db9234a0249e7f70c511071f654e8b
김우원(Kim Woowon) <dyffh1031@gmail.com>
2023-03-27 15:18:30 +0900

    [Silver II] Title: 뒤집어진 소수, Time: 1864 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        System.out.println(isSosu(N) && isFlipSosu(N) ? "yes" : "no");
+    }
+
+    private static boolean isFlipSosu(long n) {
+        String[] flipNum = {"0","1","2","e","e","5","9","e","8","6"};
+        char[] origin = String.valueOf(n).toCharArray();
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = origin.length - 1; i >= 0; i--) {
+            sb.append(flipNum[origin[i] - '0']);
+        }
+
+        try {
+            long flip = Long.parseLong(sb.toString());
+            return isSosu(flip);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static boolean isSosu(long n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return n != 1;
+    }
+
+}
