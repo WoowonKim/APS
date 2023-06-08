05a250495c0701cfbe9b7dd3482f1f2400d2c84a
김우원(Kim Woowon) <dyffh1031@gmail.com>
2023-06-08 19:53:36 +0900

    [Bronze IV] Title: 11 月 (November), Time: 216 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/27332. 11 月 （November）/11 月 （November）.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a + b * 7 < 31) System.out.println(1);
+        else System.out.println(0);
+    }
+}
