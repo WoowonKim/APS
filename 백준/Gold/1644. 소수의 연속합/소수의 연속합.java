fab7c12f9c12e305a5db250ac7a4898d37fbfcd9
김우원(Kim Woowon) <dyffh1031@gmail.com>
2023-07-21 11:03:47 +0900

    [Gold III] Title: 소수의 연속합, Time: 404 ms, Memory: 34564 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    static final int MAX = 4_000_000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> primes = new ArrayList<>();
+        boolean[] isPrime = new boolean[MAX + 1];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i * i <= MAX; i++) {
+            if (isPrime[i]) {
+                for (int p = i * i; p <= MAX; p += i) isPrime[p] = false;
+            }
+        }
+        for (int i = 2; i <= MAX; i++) {
+            if (isPrime[i]) primes.add(i);
+        }
+        int left = 0, right = 0;
+        int ans = 0, sum = 2;
+        int size = primes.size();
+        while (left < size) {
+            if (sum == n) {
+                ans++;
+                sum -= primes.get(left++);
+            } else if (sum > n) {
+                sum -= primes.get(left++);
+            } else {
+                if (++right >= size) break;
+                sum += primes.get(right);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
